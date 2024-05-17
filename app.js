214119ce0e17b583ddd450909bb668496684e83b
tabasann <steammonkey13@gmail.com>
2024-05-18 02:07:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,70 +1,49 @@
 // app.js
-
-function setBadge(value) {
-   if ('setAppBadge' in navigator) {
-       navigator.setAppBadge(value);
-   } else {
-       console.log('setAppBadge is not supported.');
-   }
-}
-
-function clearBadge() {
-   if ('clearAppBadge' in navigator) {
-       navigator.clearAppBadge();
-   } else {
-       console.log('clearAppBadge is not supported.');
-   }
-}
-
 function omikuji() {
-   let msg1;
-   let msg2;
-   let random1;
-   let random2;  
-   random1 = Math.floor(Math.random() * 5);
-   console.log(random1);
+  let msg1;
+  let msg2;
+  let random1;
+  let random2;  
+  random1 = Math.floor(Math.random() * 5);
+  console.log(random1);
 
-   const result = ['大吉', '中吉', '吉', '凶', '大凶'];
+  const result = ['大吉', '中吉', '吉', '凶', '大凶'];
 
-   msg1 = result[random1];
+  msg1 = result[random1];
 
-   random2 = Math.floor(Math.random() * 10);
-   console.log(random2);
+  random2 = Math.floor(Math.random() * 10);
+  console.log(random2);
 
-   const resultMsg = [
-       '今日はいいこと起きそう',
-       'コーラを呑みましょう。',
-       'ポテチを食べましょう。',
-       'バックアップを取っておきましょう。',
-       '忘れ物をしないように気をつけてください。',
-       '怪我には気をつけてください。',
-       'なくしたものが見つかるでしょう。',
-       'お金を無くすでしょう。',
-       '電車が遅延するでしょう。',
-       'スタバの新作を飲みましょう。',
-   ];
+  const resultMsg = [
+      '今日はいいこと起きそう',
+      'コーラを呑みましょう。',
+      'ポテチを食べましょう。',
+      'バックアップを取っておきましょう。',
+      '忘れ物をしないように気をつけてください。',
+      '怪我には気をつけてください。',
+      'なくしたものが見つかるでしょう。',
+      'お金を無くすでしょう。',
+      '電車が遅延するでしょう。',
+      'スタバの新作を飲みましょう。',
+  ];
 
-   msg2 = resultMsg[random2];
+  msg2 = resultMsg[random2];
 
-   setBadge(random1); // バッジを設定
-
-   document.getElementById('msgHeading').setAttribute('style', 'opacity: 1;');
-   setTimeout(function() {
-       document.getElementById('msg1').innerHTML = msg1;
-   }, 1000);
-   setTimeout(function() {
-       document.getElementById('msg2').innerHTML = msg2;
-   }, 2000);
-   document.getElementById('playBtn').setAttribute('disabled', 'disabled');
-   document.getElementById('resetBtn').removeAttribute('disabled');
+  document.getElementById('msgHeading').setAttribute('style', 'opacity: 1;');
+  setTimeout(function() {
+      document.getElementById('msg1').innerHTML = msg1;
+  }, 1000);
+  setTimeout(function() {
+      document.getElementById('msg2').innerHTML = msg2;
+  }, 2000);
+  document.getElementById('playBtn').setAttribute('disabled', 'disabled');
+  document.getElementById('resetBtn').removeAttribute('disabled');
 }
 
 function reset() {
-   clearBadge(); // バッジをクリア
-
-   document.getElementById('msgHeading').setAttribute('style', 'opacity: 0;');
-   document.getElementById('msg1').innerHTML = '';
-   document.getElementById('msg2').innerHTML = '';
-   document.getElementById('playBtn').removeAttribute('disabled');
-   document.getElementById('resetBtn').setAttribute('disabled', 'disabled');
+  document.getElementById('msgHeading').setAttribute('style', 'opacity: 0;');
+  document.getElementById('msg1').innerHTML = '';
+  document.getElementById('msg2').innerHTML = '';
+  document.getElementById('playBtn').removeAttribute('disabled');
+  document.getElementById('resetBtn').setAttribute('disabled', 'disabled');
 }
