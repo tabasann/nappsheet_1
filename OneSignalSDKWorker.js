61bf65ce435c60c12a0bf1e0c071495dccd42c96
tabasann <steammonkey13@gmail.com>
2024-05-17 15:48:30 +0900

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,2 +1,1 @@
-importScripts('serviceWorker.js');
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
