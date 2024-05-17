94935367b6f2828cac6c10f8357ab08eb175110b
tabasann <steammonkey13@gmail.com>
2024-05-17 16:04:49 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -7,21 +7,17 @@ var urlsToCache = [
 // インストール処理
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
 // リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
+        caches.match(event.request).then(function(response) {
+            return response ? response : fetch(event.request);
+        })
     );
 });
