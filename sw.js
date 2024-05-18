95a086ab6cc781fca06f18b474fa372e67945191
tabasann <steammonkey13@gmail.com>
2024-05-18 12:33:23 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -25,15 +25,8 @@ self.addEventListener('push', function(event) {
 setBadge();
 });
 
-// 初期値をローカルストレージから取得する。保存された値がない場合は初期値を使用する
-let currentBadgeValue = localStorage.getItem('badgeValue') ? parseInt(localStorage.getItem('badgeValue')) : 0;
 
 function setBadge() {
-    // バッジの値を更新する処理
-    currentBadgeValue += 1;
-
-    // 更新したバッジの値をローカルストレージに保存する
-    localStorage.setItem('badgeValue', currentBadgeValue);
 
     // バッジを設定する処理
     if ('setAppBadge' in navigator) {
