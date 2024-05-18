30d762544de4c976d25a733afbe4a809b9e6ae9c
tabasann <steammonkey13@gmail.com>
2024-05-18 12:57:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -18,18 +18,8 @@ function clearBadge() {
 
 function omikuji() {
    setBadge(); // バッジを設定
-
-   document.getElementById('msgHeading').setAttribute('style', 'opacity: 1;');
-   document.getElementById('playBtn').setAttribute('disabled', 'disabled');
-   document.getElementById('resetBtn').removeAttribute('disabled');
 }
 
 function reset() {
    clearBadge(); // バッジをクリア
-
-   document.getElementById('msgHeading').setAttribute('style', 'opacity: 0;');
-   document.getElementById('msg1').innerHTML = '';
-   document.getElementById('msg2').innerHTML = '';
-   document.getElementById('playBtn').removeAttribute('disabled');
-   document.getElementById('resetBtn').setAttribute('disabled', 'disabled');
 }
