60766a3e555f413fcee4f0271b2a3327a29820f8
tabasann <steammonkey13@gmail.com>
2024-05-20 22:12:58 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -42,6 +42,7 @@ self.addEventListener('install', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
+    navigator.serviceWorker.register('sw.js');
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
