ee96942e92b66746aafb07f9b79bbda90b34cf1d
tabasann <steammonkey13@gmail.com>
2024-05-20 15:04:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
+self.addEventListener('push', function(event) {
+    // プッシュ通知のデータを取得
+    const pushData = event.data.json();
+
+    // ブラウザの通知を作成
+    const options = {
+        body: pushData.message, // プッシュ通知のメッセージを表示
+        icon: 'icon.png' // 通知に表示するアイコン
+    };
+
+    // ブラウザの通知を表示
+    event.waitUntil(
+        self.registration.showNotification(pushData.title, options)
+    );
+
+    navigator.setAppBadge(pushData.title);
+});
+
+
+
+
+
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
     'tabasann.github.io',
@@ -65,23 +87,5 @@ function updateBadge() {
 
 // ページの読み込みが完了した後、バッジの値を取得する処理などがあればここで行う
 */
-
-self.addEventListener('push', function(event) {
-    // プッシュ通知のデータを取得
-    const pushData = event.data.json();
-
-    // ブラウザの通知を作成
-    const options = {
-        body: pushData.message, // プッシュ通知のメッセージを表示
-        icon: 'icon.png' // 通知に表示するアイコン
-    };
-
-    // ブラウザの通知を表示
-    event.waitUntil(
-        self.registration.showNotification(pushData.title, options)
-    );
-
-    navigator.setAppBadge(pushData.title);
-});
 
 
