85fe0ea7f2b7022745e731701f85263a38b69d3f
tabasann <steammonkey13@gmail.com>
2024-05-20 22:02:19 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -34,11 +34,11 @@ self.addEventListener('install', function(event) {
     );
 });
 
-self.addEventListener('fetch', function(event) {
+/*self.addEventListener('fetch', function(event) {
     event.respondWith(
         fetch(event.request)
     );
-});
+});*/
 
 
 self.addEventListener('fetch', function(event) {
