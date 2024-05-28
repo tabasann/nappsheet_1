ca41e676af01bf2b49abdf9f2b0c7e102e0d6300
tabasann <steammonkey13@gmail.com>
2024-05-28 21:53:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -26,6 +26,11 @@ function refresh() {
   navigator.serviceWorker.register('sw.js');
 }
 
+function appsheet() {
+  navigator.serviceWorker.register('sw.js');
+  window.location.href = "https://www.google.co.jp/";
+}
+
 function load() {
   window.location.href = "https://tabasann.github.io/";
 }
